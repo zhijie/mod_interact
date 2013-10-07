e4579616eb98280de880c3207a2d1348aa312978
Adam Duke <adam.v.duke@gmail.com>
2013-10-07 17:11:13 -0400

    url encode the post body before sending

--- src/mod_offline_post.erl
+++ src/mod_offline_post.erl
@@ -68,7 +68,7 @@ send_notice(_From, To, Packet) ->
 	  Post = [
 	    "to=", To#jid.luser, Sep,
 	    "from=", _From#jid.luser, Sep,
-	    "body=", Body, Sep,
+	    "body=", url_encode(Body), Sep,
 	    "access_token=", Token ],
 	  ?INFO_MSG("Sending post request ~p~n",[Post] ),
 	  httpc:request(post, {PostUrl, [], "application/x-www-form-urlencoded", list_to_binary(Post)},[],[]),
@@ -76,4 +76,47 @@ send_notice(_From, To, Packet) ->
 	true ->
 	  ok
     end.
+
+
+%%% The following url encoding code is from the yaws project and retains it's original license.
+%%% https://github.com/klacke/yaws/blob/master/LICENSE
+%%% Copyright (c) 2006, Claes Wikstrom, klacke@hyber.org
+%%% All rights reserved.
+url_encode([H|T]) when is_list(H) ->
+    [url_encode(H) | url_encode(T)];
+url_encode([H|T]) ->
+    if
+        H >= $a, $z >= H ->
+            [H|url_encode(T)];
+        H >= $A, $Z >= H ->
+            [H|url_encode(T)];
+        H >= $0, $9 >= H ->
+            [H|url_encode(T)];
+        H == $_; H == $.; H == $-; H == $/; H == $: -> % FIXME: more..
+            [H|url_encode(T)];
+        true ->
+            case integer_to_hex(H) of
+                [X, Y] ->
+                    [$%, X, Y | url_encode(T)];
+                [X] ->
+                    [$%, $0, X | url_encode(T)]
+            end
+     end;
+
+url_encode([]) ->
+    [].
+
+integer_to_hex(I) ->
+    case catch erlang:integer_to_list(I, 16) of
+        {'EXIT', _} -> old_integer_to_hex(I);
+        Int         -> Int
+    end.
+
+old_integer_to_hex(I) when I < 10 ->
+    integer_to_list(I);
+old_integer_to_hex(I) when I < 16 ->
+    [I-10+$A];
+old_integer_to_hex(I) when I >= 16 ->
+    N = trunc(I/16),
+    old_integer_to_hex(N) ++ old_integer_to_hex(I rem 16).
 
