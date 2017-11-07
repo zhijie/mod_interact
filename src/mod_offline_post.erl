18e39c4619f6d0b266f8ead0e45ba81ec17f4613
onezeros <onezeros@qq.com>
2017-11-08 01:10:07 +0800

    debug

--- src/mod_offline_post.erl
+++ src/mod_offline_post.erl
@@ -63,7 +63,7 @@ send_notice({_Action, #message{type = Type, body = Body, to = To, from = From}} 
     Token = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, auth_token, fun(S) -> iolist_to_binary(S) end, list_to_binary("")),
     PostUrl = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, post_url, fun(S) -> iolist_to_binary(S) end, list_to_binary("")),
 	?INFO_MSG("Type----------: Type:~s ", [Type]),
-    if (Type == chat) and (Body /= <<"">>) ->
+    if string:equal(Type == <<"chat">>) and (Body /= <<"">>) ->
 	      Sep = "&",
         Post = [
           "to=", To#jid.luser, Sep,
