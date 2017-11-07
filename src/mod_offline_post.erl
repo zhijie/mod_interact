67c7aa229bb2c2e14cb2f0bb18c8b94b4468b772
onezeros <onezeros@qq.com>
2017-11-08 02:52:18 +0800

    debug

--- src/mod_offline_post.erl
+++ src/mod_offline_post.erl
@@ -65,7 +65,7 @@ send_notice({_Action, #message{type = Type, body = Body, to = To, from = From}} 
     ?INFO_MSG("Sending post request to ~s with Token \"~s\"", [PostUrl, Token]),
 	?INFO_MSG("Type----------: Type:~s ", [Type]),
 	?INFO_MSG("Body is_list----------: ~s ", [(is_list(Body))]),
-	io:format("Body ~p~n",[Body]),
+	io:format("Body ~p~n",[lists:last(Body)]),
     if (Type == chat) and (Body /= <<"">>) ->
 		Sep = "&",
 	    data = Body#text.data,
