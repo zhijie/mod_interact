5fbec1318bab5e0b7b35abd0d66266ab8f550d25
onezeros <onezeros@qq.com>
2017-11-14 13:40:05 +0800

    debugging group message push

--- src/mod_jpush.erl
+++ src/mod_jpush.erl
@@ -130,7 +130,9 @@ send_notice({_Action, Message} = Acc) ->
         Acc;
       (Type == normal) and (ServerPrefix /= nomatch) ->
       	Els = xmpp:get_els(Message),
-        io:format("Els : ~p~n",[Els]),
+      	[PsEvent|_] = Els,
+      	{_, PsItems} = PsEvent,
+        io:format("PsItems : ~p~n",[PsItems]),
       	Acc;
       true ->
         Acc
