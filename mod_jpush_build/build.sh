012f994596a07cd1ca84dd90cb3d006e2c1db05e
win7 onezeros <onezeros.lee@gmail.com>
2017-12-16 17:10:16 +0800

    windows version build ok

--- mod_jpush_build/build.sh
+++ mod_jpush_build/build.sh
@@ -1,2 +1,2 @@
-erl -pa /mnt/ejabberd/ejabberd1707/deps/lager/ebin/  -pz ebin -make
+erl -pa C:\Program\ Files\ejabberd-17.07\lib\lager-3.4.2\ebin -pa C:\Program\ Files\ejabberd-17.07\lib\stdlib-3.3\ebin  -pz ebin -make
 
